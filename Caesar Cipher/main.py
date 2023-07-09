1da391c3c77f68ea2f5e833832617a62c8aba498
Sachin Sharma <113278199+Moat6@users.noreply.github.com>
2023-07-09 23:39:13 +0530

    Add files via upload

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -1,1 +1,27 @@
+import art
+print(art.logo)
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']*2
+
+def code():
+    global alphabet
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = (int(input("Type the shift number:\n"))) % 26
+    def cypher(text,shift,direction) :
+        new_text=""
+        if direction=="decode" :
+            shift *= -1            
+        for char in text :
+            if char in alphabet:
+                position=alphabet.index(char)
+                new_text += alphabet[position+shift]                
+            else:
+                new_text+= char
+                
+        print(f"The {direction}d text is {new_text}")    
+    cypher(text,shift,direction)   
+    choice=input("Do you want to run the code again(y/n) :")
+    if choice in "Yy":
+        code()
+code()        
