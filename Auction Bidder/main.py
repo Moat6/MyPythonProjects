99d3e8e2805456ba3521bd6e4019e2bd0e1b2e0a
Sachin Sharma <113278199+Moat6@users.noreply.github.com>
2023-07-09 23:35:18 +0530

    Add files via upload

--- Auction Bidder/main.py
+++ Auction Bidder/main.py
@@ -1,1 +1,22 @@
+import os
 
+print(f"Welcome to auction bidder")
+bidders={}
+while True:
+    name=input("Enter name of the bidder :")
+    bid=int(input("Enter the bidding amount :"))
+    bidders[name]=bid
+    new=input("Are there more bidders? (y/n) :")
+    if new=="y" :
+        os.system("cls")
+        continue
+    else :
+        break
+for name in bidders :
+    highest_bid=0
+    winner=None
+    if bidders[name]>highest_bid :
+        highest_bid=bidders[name]
+        winner=name
+
+print(f"Highest Bidder is {winner} with bid of {highest_bid}")
